b014135e2ecfab0ef4a37cd868facf44da245a74
andrey <naumov.a.n@icloud.com>
2023-09-30 18:42:18 +0700

    hw1. Задание 2. Графический интерфейс клиента. v.02

--- src/main/java/ru/gb/server/ClientGUI.java
+++ src/main/java/ru/gb/server/ClientGUI.java
@@ -2,10 +2,7 @@ package ru.gb.server;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.FocusEvent;
-import java.awt.event.FocusListener;
+import java.awt.event.*;
 
 public class ClientGUI extends JFrame {
     private static final int WIDTH = 600;
@@ -23,12 +20,8 @@ public class ClientGUI extends JFrame {
         btnLogin = new JButton("Логин");
         btnToSend = new JButton("Отправить");
 
-        JTextField ipText = new JTextField();
-        JTextField portText = new JTextField();
-        JTextField loginText = new JTextField();
-        JTextField passwordText = new JTextField();
-        JTextField mesageText = new JTextField();
-
+        JTextArea logTextArea = new JTextArea();
+        logTextArea.setEditable(false);
         btnLogin.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -36,46 +29,138 @@ public class ClientGUI extends JFrame {
             }
         });
 
+
+
+        add(createALoginPanel(), BorderLayout.NORTH);
+        add(logTextArea);
+        add(sendingMessages(), BorderLayout.SOUTH);
+
+        setVisible(true);
+    }
+
+    private Component sendingMessages() {
+        JPanel entryPanel = new JPanel(new GridLayout(0,2));
+        JTextArea textArea = new JTextArea();
+//        textArea.setLineWrap(true); //перенос строк
+//        textArea.setWrapStyleWord(true); //перенос слов
+        JScrollPane jScrollPane = new JScrollPane(textArea); //скролинг
+//        jScrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_NEVER);
+
+        entryPanel.add(jScrollPane, BorderLayout.CENTER);
+        entryPanel.add(btnToSend, BorderLayout.EAST);
+
+        textArea.setText("Введите cсообщение");
+        textArea.addFocusListener(new FocusListener() {
+
+            @Override
+            public void focusGained(FocusEvent e) {
+                if (textArea.getText().equals("Введите cсообщение")) {
+                    textArea.setText("");
+                }
+            }
+
+            @Override
+            public void focusLost(FocusEvent e) {
+                if (textArea.getText().isEmpty()) {
+                    textArea.setText("Введите cсообщение");
+                }
+            }
+        });
+
         btnToSend.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
+                String message = textArea.getText();
+                //здесь хочу обратиться к  logTextArea (который инициализирован на 23 строке)  и присвоить, что то вроде этого:
+                logTextArea.append(message + "\n");
+                textArea.setText("");
 
             }
         });
 
-        JPanel entryPanel = new JPanel(new GridLayout(2,3));
-        entryPanel.add(ipText);
-        ipText.setForeground(Color.GRAY);
+        return entryPanel;
+    }
+
+    private Component createALoginPanel(){
+        JPanel entryPanel = new JPanel(new GridLayout(2,1));
+        entryPanel.add(createIpPanel());
+        entryPanel.add(createLoginPanel());
+
+        return entryPanel;
+    }
+
+    private Component createIpPanel() {
+        JPanel panel = new JPanel(new GridLayout(1,2));
+        JTextField ipText = new JTextField();
+        JTextField portText = new JTextField();
+        panel.add(ipText);
+        panel.add(portText);
+
         ipText.setText("Введите ip сервера");
-        ipText.addFocusListener(new FocusListener() {
+        ipText.addFocusListener(new FocusAdapter() {
             @Override
             public void focusGained(FocusEvent e) {
                 if (ipText.getText().equals("Введите ip сервера")) {
-                    ipText.setText("");
-                    ipText.setForeground(Color.BLACK);
+                    ipText.setText("127.0.0.1");
                 }
             }
 
             @Override
             public void focusLost(FocusEvent e) {
                 if (ipText.getText().isEmpty()) {
-                    ipText.setForeground(Color.GRAY);
                     ipText.setText("Введите ip сервера");
                 }
             }
         });
 
-        entryPanel.add(portText);
-        entryPanel.add(loginText);
-        entryPanel.add(passwordText);
-        entryPanel.add(btnLogin);
+        portText.setText("Введите PORT сервера");
+        portText.addFocusListener(new FocusAdapter() {
+            @Override
+            public void focusGained(FocusEvent e) {
+               if(portText.getText().equals("Введите PORT сервера")){
+                   portText.setText("8080");
+               }
+            }
 
-        add(entryPanel, BorderLayout.NORTH);
+            @Override
+            public void focusLost(FocusEvent e) {
+                if (portText.getText().isEmpty()) {
+                    portText.setText("Введите PORT сервера");
+                }
+            }
+        });
 
-        setVisible(true);
+        return panel;
     }
 
-    private Component createEntryPanel(){
+    private Component createLoginPanel() {
+        JPanel panel = new JPanel(new GridLayout(1,3));
 
+        JTextField loginText = new JTextField();
+        JPasswordField passwordText = new JPasswordField();
+        passwordText.setEchoChar('*');
+
+        panel.add(loginText);
+        panel.add(passwordText);
+        panel.add(btnLogin);
+
+        loginText.setText("Введите логин");
+        loginText.addFocusListener(new FocusAdapter() {
+            @Override
+            public void focusGained(FocusEvent e) {
+                if(loginText.getText().equals("Введите логин")){
+                    loginText.setText("Admin");
+                }
+            }
+
+            @Override
+            public void focusLost(FocusEvent e) {
+                if (loginText.getText().isEmpty()) {
+                    loginText.setText("Введите логин");
+                }
+            }
+        });
+
+        return panel;
     }
 }
---
+++ src/main/java/ru/gb/server/Main.java
@@ -0,0 +1,10 @@
+package ru.gb.server;
+
+import ru.gb.server.ServerWindow;
+
+public class Main {
+    public static void main(String[] args) {
+        //new ServerWindow();
+        new ClientGUI();
+    }
+}
