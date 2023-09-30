91bfc75db6bf8a23b534ae4b4bf15b0e2f3aef77
andrey <naumov.a.n@icloud.com>
2023-09-30 19:17:51 +0700

    hw1. Задание 2. изменена кнопка старта

--- src/main/java/ru/gb/server/ServerWindow.java
+++ src/main/java/ru/gb/server/ServerWindow.java
@@ -1,6 +1,8 @@
 package ru.gb.server;
 
 import javax.swing.*;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -10,7 +12,8 @@ public class ServerWindow extends JFrame {
     private static final int WIDTH = 600;
     private static final int HEIGHT = 550;
 
-    JButton btnStart, btnStop;
+    JButton btnStop;
+    JToggleButton btnStart;
 
     public ServerWindow() throws HeadlessException{
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //остановитть при закрытии
@@ -19,8 +22,19 @@ public class ServerWindow extends JFrame {
         setTitle("Сервер"); //заголовок
         setResizable(false); //не изменяемое окно
 
-        btnStart = new JButton("Старт");
-        btnStop = new JButton("Стоп");
+        btnStart = new JToggleButton("Влючить");
+        btnStart.addChangeListener(new ChangeListener() {
+            @Override
+            public void stateChanged(ChangeEvent e) {
+                if (btnStart.isSelected()) {
+                    btnStart.setText("Отключить");
+                } else {
+                    btnStart.setText("Включить");
+                }
+            }
+        });
+
+        btnStop = new JButton("Выход");
 
         JTextArea textArea = new JTextArea(); //мнгострочное текстовое поле
 
